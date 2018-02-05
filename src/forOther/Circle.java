702dabb55868b9af38e03e10e7f3752268e20d48
Maxim Bubnov <soby321@yandex.ru>
2018-02-05 14:12:43 +0300

    Circle for Labs

---
+++ src/forOther/Circle.java
@@ -0,0 +1,52 @@
+package forOther;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Circle implements ActionListener {
+
+    JFrame frame;
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.go();
+    }
+
+    public void go() {
+        frame = new JFrame("Example");
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+
+        JButton button = new JButton("Change color");
+        button.addActionListener(this);
+
+        MyDrawPanel drawPanel = new MyDrawPanel();
+
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.getContentPane().add(BorderLayout.CENTER, drawPanel );
+        frame.setSize(400,400);
+        frame.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        frame.repaint();
+    }
+
+}
+
+class MyDrawPanel extends JPanel {
+
+    @Override
+    protected void paintComponent(java.awt.Graphics g) {
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+        int red = (int) (Math.random() * 255);
+        int green = (int) (Math.random() * 255);
+        int blue = (int) (Math.random() * 255);
+
+        Color randomColor = new Color(red, green, blue);
+        g.setColor(randomColor);
+        g.fillOval(70, 70, 100, 100);
+    }
+}
